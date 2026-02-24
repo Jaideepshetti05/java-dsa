33b329aa53af48e685797981002cb4faf6359eaf
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-24 20:53:57 +0530

    commit 4

---
+++ feb24/StringRotation.java
@@ -0,0 +1,13 @@
+public class StringRotation {
+
+    public static boolean isRotation(String s1, String s2) {
+        if(s1.length() != s2.length())
+            return false;
+
+        return (s1 + s1).contains(s2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isRotation("abcd", "cdab"));
+    }
+}
