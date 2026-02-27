8ab2e39ad1972d5734357c7c84bd387d13652e91
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-27 20:57:09 +0530

    commit 7

---
+++ feb27/MaxSubarraySum.java
@@ -0,0 +1,12 @@
+public class MaxSubarraySum {
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int current = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            current = Math.max(nums[i], current + nums[i]);
+            max = Math.max(max, current);
+        }
+        return max;
+    }
+}
