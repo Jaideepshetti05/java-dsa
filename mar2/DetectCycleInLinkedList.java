ced502cfeb0a168496b6210c934fe23db5a8ccee
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-02 20:22:10 +0530

    commit 6

---
+++ mar2/DetectCycleInLinkedList.java
@@ -0,0 +1,17 @@
+class Node {
+    int data;
+    Node next;
+    Node(int d) { data = d; next = null; }
+}
+
+public class DetectCycleInLinkedList {
+    static boolean hasCycle(Node head) {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
