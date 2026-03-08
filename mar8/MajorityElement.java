829aa210adeb887c77e562d13ac2de284bd01204
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-08 21:36:10 +0530

    commit 1

---
+++ mar8/MajorityElement.java
@@ -0,0 +1,20 @@
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+
+            if (num == candidate)
+                count++;
+            else
+                count--;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println("Majority Element: " + majorityElement(arr));
+    }
+}
