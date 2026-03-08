a1cbee4cee950c6eca9de85491a9efe28098461d
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-08 21:36:53 +0530

    commit 2

---
+++ mar8/MoveZeros.java
@@ -0,0 +1,24 @@
+public class MoveZeros {
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
+
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,0,3,12};
+
+        moveZeroes(arr);
+
+        for (int n : arr)
+            System.out.print(n + " ");
+    }
+}
