dffa1314fe07d0fe4d68cde4b34135d617346391
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-10 19:49:23 +0530

    commit 3

---
+++ mar10/RemoveDuplicatesArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class RemoveDuplicatesArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,4,4,5};
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for(int i:arr)
+            set.add(i);
+
+        System.out.println(set);
+    }
+}
