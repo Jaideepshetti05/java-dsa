3f45fe889ea30b811a566d6eac458c85a283c51e
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-11 21:50:41 +0530

    commit 4

---
+++ mar11/AnagramCheck.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+    public static boolean isAnagram(String a, String b) {
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen","silent"));
+    }
+}
