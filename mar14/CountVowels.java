fd9e5dcc0c5f5a62c98ce5778c6bb7a71806a88b
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-14 19:19:44 +0530

    commit 6

---
+++ mar14/CountVowels.java
@@ -0,0 +1,17 @@
+public class CountVowels {
+
+    public static void main(String[] args) {
+
+        String str="education";
+
+        int count=0;
+
+        for(char c:str.toCharArray()){
+
+            if("aeiouAEIOU".indexOf(c)!=-1)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
