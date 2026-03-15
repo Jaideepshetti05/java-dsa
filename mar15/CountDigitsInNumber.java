54f9a5ac3b50641318c387d20779569a6a2804ba
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-15 21:33:54 +0530

    commit 6

---
+++ mar15/CountDigitsInNumber.java
@@ -0,0 +1,17 @@
+public class CountDigitsInNumber {
+
+    public static int countDigits(int n){
+        int count = 0;
+
+        while(n > 0){
+            n /= 10;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args){
+        System.out.println(countDigits(123456));
+    }
+}
