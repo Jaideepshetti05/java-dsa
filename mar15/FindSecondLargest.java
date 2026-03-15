8cf51b7bc6f15d7a475f95541ed957df7197deaf
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-15 21:34:15 +0530

    commit 7

---
+++ mar15/FindSecondLargest.java
@@ -0,0 +1,23 @@
+public class FindSecondLargest {
+
+    public static int secondLargest(int[] arr){
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int n : arr){
+            if(n > first){
+                second = first;
+                first = n;
+            }
+            else if(n > second && n != first)
+                second = n;
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {10,20,4,45,99};
+        System.out.println(secondLargest(arr));
+    }
+}
