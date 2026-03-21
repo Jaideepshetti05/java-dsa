bb0963af211e990803e0c678c5a3f66d48fd41e3
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-21 19:18:01 +0530

    commit 7

---
+++ mar21/TrieImplementation.java
@@ -0,0 +1,18 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEnd = false;
+}
+
+public class TrieImplementation {
+    TrieNode root = new TrieNode();
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null)
+                node.children[c - 'a'] = new TrieNode();
+            node = node.children[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+}
