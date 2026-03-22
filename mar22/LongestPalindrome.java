8a634f7fe5cb653f747bd430dae6bb07fe170868
Jaideep <jaideepshetti55@gmail.com>
2026-03-22 17:16:04 +0530

    commit 3

---
+++ mar22/LongestPalindrome.java
@@ -0,0 +1,24 @@
+// File: LongestPalindrome.java
+public class LongestPalindrome {
+    public static String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String odd = expand(s, i, i);
+            String even = expand(s, i, i + 1);
+            if (odd.length() > res.length()) res = odd;
+            if (even.length() > res.length()) res = even;
+        }
+        return res;
+    }
+
+    static String expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--; r++;
+        }
+        return s.substring(l + 1, r);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+}
