4e27f6be2a48d761c42c052b0b2b4fe0919e5e02
Jaideep <jaideepshetti55@gmail.com>
2026-04-01 20:15:47 +0530

    commit 6

---
+++ apr01/KthLargestElement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class KthLargestElement {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : nums) {
+            pq.add(num);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
