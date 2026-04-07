a2e6b5436006f934e01399b57ef3ff6f637e480a
Jaideep <jaideepshetti55@gmail.com>
2026-04-07 23:26:05 +0530

    commit 10

---
+++ apr07/CheckAnagram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CheckAnagram {
+    public static boolean isAnagram(String a, String b) {
+        char[] c1 = a.toCharArray();
+        char[] c2 = b.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        return Arrays.equals(c1, c2);
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+    }
+}
