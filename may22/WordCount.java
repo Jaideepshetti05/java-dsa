423b5a19e0d544f2fd0355fff604f42f4b7a26bb
Jaideep <jaideepshetti55@gmail.com>
2026-05-22 21:37:07 +0530

    commit 2

---
+++ may22/WordCount.java
@@ -0,0 +1,15 @@
+// File: WordCount.java
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.trim().split("\\s+");
+
+        System.out.println("Word Count: " + words.length);
+    }
+}
