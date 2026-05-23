e9c4dcb79b4792f9929108a0580d1792d49017b3
Jaideep <jaideepshetti55@gmail.com>
2026-05-23 22:07:18 +0530

    commit 7

---
+++ may23/ManualSort.java
@@ -0,0 +1,21 @@
+public class ManualSort {
+    public static void main(String[] args) {
+        int arr[] = {8,3,1,6,2};
+
+        for(int i=0;i<arr.length;i++) {
+            for(int j=i+1;j<arr.length;j++) {
+
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for(int num : arr)
+            System.out.print(num + " ");
+    }
+}
