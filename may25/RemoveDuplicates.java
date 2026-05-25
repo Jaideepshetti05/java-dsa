4c7406f7abbd1c6abfe910231489fd7ae8ed4ed0
Jaideep <jaideepshetti55@gmail.com>
2026-05-25 22:00:45 +0530

    commit 8

---
+++ may25/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+// File: RemoveDuplicates.java
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        String str = "programming";
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+
+        for (char ch : set) {
+            System.out.print(ch);
+        }
+    }
+}
