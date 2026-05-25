50d2f414884cfe75062808c734ad8ab9d3c89e80
Jaideep <jaideepshetti55@gmail.com>
2026-05-25 21:59:14 +0530

    commit 6

---
+++ may25/SumOdd.java
@@ -0,0 +1,13 @@
+// File: SumOdd.java
+public class SumOdd {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= 50; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
