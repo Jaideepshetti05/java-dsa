84d164b33696419c965455391a80d25158cf84bb
Jaideep <jaideepshetti55@gmail.com>
2026-05-25 21:57:54 +0530

    commit 4

---
+++ may25/LargestArray.java
@@ -0,0 +1,17 @@
+// File: LargestArray.java
+public class LargestArray {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 23};
+
+        int max = arr[0];
+
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("Largest: " + max);
+    }
+}
