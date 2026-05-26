b476ceea69e8d16513acd070318e3c3cfaa5a5ec
Jaideep <jaideepshetti55@gmail.com>
2026-05-26 21:30:13 +0530

    commit 1

---
+++ may26/PrimeChecker.java
@@ -0,0 +1,21 @@
+// File: PrimeChecker.java
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean prime = true;
+
+        if (n <= 1) prime = false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        System.out.println(prime ? "Prime" : "Not Prime");
+    }
+}
