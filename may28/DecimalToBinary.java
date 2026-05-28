a20f83336871dbd2c74c8b05a5e07080a623ba9e
Jaideep <jaideepshetti55@gmail.com>
2026-05-28 21:12:00 +0530

    commit 9

---
+++ may28/DecimalToBinary.java
@@ -0,0 +1,9 @@
+class DecimalToBinary {
+    public static void main(String[] args) {
+        int num = 10;
+
+        String binary = Integer.toBinaryString(num);
+
+        System.out.println("Binary Value: " + binary);
+    }
+}
