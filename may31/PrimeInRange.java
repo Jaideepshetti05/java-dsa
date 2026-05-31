0f85c62fd81a80dbe9bf9fda6559ddd9bf4d5071
Jaideep <jaideepshetti55@gmail.com>
2026-05-31 23:30:44 +0530

    commit 4

---
+++ may31/PrimeInRange.java
@@ -0,0 +1,17 @@
+public class PrimeInRange {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 50; i++) {
+            boolean prime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+                System.out.print(i + " ");
+        }
+    }
+}
