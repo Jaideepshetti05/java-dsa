aae0419daa7d1bb373074d0bdc75173e65eda265
Jaideep <jaideepshetti55@gmail.com>
2026-06-01 21:04:03 +0530

    commit 3

---
+++ jun01/BinaryToDecimal.java
@@ -0,0 +1,8 @@
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "101101";
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println("Decimal: " + decimal);
+    }
+}
