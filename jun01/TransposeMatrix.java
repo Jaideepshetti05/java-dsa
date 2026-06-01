dbeb4cbf485af3c9be3ba8eaada8a5e0acd9705c
Jaideep <jaideepshetti55@gmail.com>
2026-06-01 21:15:18 +0530

    Add TransposeMatrix.java

---
+++ jun01/TransposeMatrix.java
@@ -0,0 +1,33 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transpose = new int[cols][rows];
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+        System.out.println("Transpose Matrix:");
+        printMatrix(transpose);
+    }
+    
+    public static void printMatrix(int[][] m) {
+        for (int[] row : m) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
