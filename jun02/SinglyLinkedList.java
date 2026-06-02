dd1446fb6f50cee820ee92fe303523bcfb89e397
Jaideep <jaideepshetti55@gmail.com>
2026-06-02 21:53:20 +0530

    commit 8

---
+++ jun02/SinglyLinkedList.java
@@ -0,0 +1,86 @@
+package jun02;
+
+public class SinglyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head = null;
+
+    // Insert a node at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert a node at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Delete a node with a specific value
+    public void deleteValue(int key) {
+        Node temp = head, prev = null;
+
+        // If head node itself holds the key
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        // Search for the key to be deleted
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // If key was not present in linked list
+        if (temp == null) return;
+
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+
+    // Display the linked list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        System.out.println("Inserting elements at end: 10, 20, 30");
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.display();
+
+        System.out.println("Inserting element at beginning: 5");
+        list.insertAtBeginning(5);
+        list.display();
+
+        System.out.println("Deleting element: 20");
+        list.deleteValue(20);
+        list.display();
+    }
+}
