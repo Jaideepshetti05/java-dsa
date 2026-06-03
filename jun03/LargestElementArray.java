3ca897d51e3a34d16be1de0f7c2e4572970db3bb
Jaideep <jaideepshetti55@gmail.com>
2026-06-03 22:15:27 +0530

    commit 3

---
+++ jun03/LargestElementArray.java
@@ -0,0 +1,13 @@
+public class LargestElementArray {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 67, 23, 89};
+
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max)
+                max = num;
+        }
+
+        System.out.println("Largest: " + max);
+    }
+}
