4012fd3a0aa006c0ae67d2cceee7a2b434f938bf
Jaideep <jaideepshetti55@gmail.com>
2026-06-03 22:17:35 +0530

    commit 9

---
+++ jun03/CharacterFrequency.java
@@ -0,0 +1,14 @@
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        char ch = 'g';
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if (c == ch)
+                count++;
+        }
+
+        System.out.println("Frequency: " + count);
+    }
+}
