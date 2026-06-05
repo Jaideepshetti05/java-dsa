89b09e38704930b8132e79ece3c5a77ca6037b8e
Jaideep <jaideepshetti55@gmail.com>
2026-06-05 20:56:10 +0530

    commit 8

---
+++ jun05/RemoveSpaces.java
@@ -0,0 +1,9 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Cloud DevOps Engineer";
+
+        str = str.replace(" ", "");
+
+        System.out.println(str);
+    }
+}
