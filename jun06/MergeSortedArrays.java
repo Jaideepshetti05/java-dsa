2d0524453adda627b833bc0ede36931c63e08677
Jaideep <jaideepshetti55@gmail.com>
2026-06-06 20:49:51 +0530

    commit 2

---
+++ jun06/MergeSortedArrays.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] a = {1,3,5};
+        int[] b = {2,4,6};
+
+        int[] c = new int[a.length + b.length];
+
+        System.arraycopy(a,0,c,0,a.length);
+        System.arraycopy(b,0,c,a.length,b.length);
+
+        Arrays.sort(c);
+
+        System.out.println(Arrays.toString(c));
+    }
+}
