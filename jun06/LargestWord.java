3c92e8111cf6ae077d3f5e01a547088b930bf8ee
Jaideep <jaideepshetti55@gmail.com>
2026-06-06 20:51:42 +0530

    commit 7

---
+++ jun06/LargestWord.java
@@ -0,0 +1,16 @@
+public class LargestWord {
+    public static void main(String[] args) {
+        String str = "Java programming language";
+
+        String[] words = str.split(" ");
+
+        String largest = "";
+
+        for(String word : words){
+            if(word.length() > largest.length())
+                largest = word;
+        }
+
+        System.out.println(largest);
+    }
+}
