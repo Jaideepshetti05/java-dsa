65d154a5f5bce7d83c457bacc652f95793e70a47
Jaideep <jaideepshetti55@gmail.com>
2026-06-09 21:33:06 +0530

    commit 8

---
+++ jun09/ArrayAverage.java
@@ -0,0 +1,10 @@
+public class ArrayAverage {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50};
+        double sum=0;
+
+        for(int n:arr) sum+=n;
+
+        System.out.println("Average: "+sum/arr.length);
+    }
+}
