5a470bfaccc2f7c9ea5a064704655b0e3ce7c927
Jaideep <jaideepshetti55@gmail.com>
2026-06-10 18:26:58 +0530

    commit 5

---
+++ jun10/RotateArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+
+        int n = arr.length;
+        int[] result = new int[n];
+
+        for(int i=0;i<n;i++)
+            result[(i+k)%n] = arr[i];
+
+        System.out.println(Arrays.toString(result));
+    }
+}
