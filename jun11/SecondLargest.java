3a5a79cb63eb64dd930ec8dbc93d0c4866baafc8
Jaideep <jaideepshetti55@gmail.com>
2026-06-11 08:15:34 +0530

    commit 5

---
+++ jun11/SecondLargest.java
@@ -0,0 +1,19 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10,50,20,90,40};
+
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+
+        for(int n:arr){
+            if(n>first){
+                second=first;
+                first=n;
+            } else if(n>second && n!=first){
+                second=n;
+            }
+        }
+
+        System.out.println(second);
+    }
+}
