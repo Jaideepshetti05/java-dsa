0ac948e1412122eaa59cda873af06bef0caca12a
Jaideep <jaideepshetti55@gmail.com>
2026-06-11 08:15:51 +0530

    commit 7

---
+++ jun11/PerfectSquareCheck.java
@@ -0,0 +1,9 @@
+public class PerfectSquareCheck {
+    public static void main(String[] args) {
+        int num = 144;
+
+        int root = (int)Math.sqrt(num);
+
+        System.out.println(root * root == num);
+    }
+}
