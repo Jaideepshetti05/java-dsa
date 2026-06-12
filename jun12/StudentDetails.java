a937acbe27a758a1997d035c200ba4f355f467a3
Jaideep <jaideepshetti55@gmail.com>
2026-06-12 08:17:44 +0530

    commit -1

---
+++ jun12/StudentDetails.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
