3870e34200643a2aaba66c2469929f25cc5717b0
Jaideep <jaideepshetti55@gmail.com>
2026-06-15 08:10:01 +0530

    commit 4

---
+++ jun15/MatrixDiagonalSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+        int sum = 0;
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                a[i][j] = sc.nextInt();
+
+        for(int i=0;i<n;i++)
+            sum += a[i][i];
+
+        System.out.println(sum);
+    }
+}
