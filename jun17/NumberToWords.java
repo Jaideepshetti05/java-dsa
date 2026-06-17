50b53c81f425fe50727501e0ca666ec92ae277e8
Jaideep <jaideepshetti55@gmail.com>
2026-06-17 08:15:00 +0530

    commit 3

---
+++ jun17/NumberToWords.java
@@ -0,0 +1,9 @@
+public class NumberToWords {
+    public static void main(String[] args) {
+        String[] words = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+
+        int num = 5;
+
+        System.out.println(words[num]);
+    }
+}
---
+++ jun17/PrimeFactors.java
@@ -0,0 +1,12 @@
+public class PrimeFactors {
+    public static void main(String[] args) {
+        int num = 84;
+
+        for(int i=2;i<=num;i++){
+            while(num%i==0){
+                System.out.print(i + " ");
+                num/=i;
+            }
+        }
+    }
+}
