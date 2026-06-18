0eab655d0ea23f1c84036f6977ea2a42428170ad
Jaideep <jaideepshetti55@gmail.com>
2026-06-18 08:05:19 +0530

    commit 2

---
+++ jun18/FileWordCounter.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class FileWordCounter {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
+        int count = 0;
+        String line;
+
+        while((line = br.readLine()) != null){
+            count += line.split("\\s+").length;
+        }
+
+        br.close();
+        System.out.println("Words: " + count);
+    }
+}
