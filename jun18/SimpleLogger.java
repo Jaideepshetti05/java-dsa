c45e29606d3f7ada9a7eedcd3faab4b081559d5e
Jaideep <jaideepshetti55@gmail.com>
2026-06-18 08:08:59 +0530

    commit 6

---
+++ jun18/SimpleLogger.java
@@ -0,0 +1,10 @@
+import java.io.FileWriter;
+
+public class SimpleLogger {
+    public static void main(String[] args) throws Exception {
+        FileWriter fw = new FileWriter("log.txt", true);
+        fw.write("Application Started\n");
+        fw.close();
+        System.out.println("Log Written");
+    }
+}
