b1b88a24998319232fba30b618a47042e6e25fe1
Jaideep <jaideepshetti55@gmail.com>
2026-06-20 18:38:28 +0530

    commit 10

---
+++ jun20/ElectricityBill.java
@@ -0,0 +1,7 @@
+public class ElectricityBill {
+    public static void main(String[] args) {
+        int units = 250;
+        double bill = units * 6.5;
+        System.out.println("Bill Amount: ₹" + bill);
+    }
+}
