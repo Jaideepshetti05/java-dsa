32c0f18cbfb388c13cac70337f77f699c6daca75
Jaideep <jaideepshetti55@gmail.com>
2026-06-21 21:14:54 +0530

    commit -7

---
+++ jun21/MatrixDiagonalSum.java
@@ -0,0 +1,11 @@
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] m = {{1,2,3},{4,5,6},{7,8,9}};
+        int sum = 0;
+
+        for(int i=0;i<3;i++)
+            sum += m[i][i];
+
+        System.out.println(sum);
+    }
+}
