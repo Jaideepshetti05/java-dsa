8532040f524a3e0d705ed8df92b6c1c9794963bf
Jaideep <jaideepshetti55@gmail.com>
2026-06-22 08:16:55 +0530

    commit 6

---
+++ jun22/LibraryFineCalculator.java
@@ -0,0 +1,9 @@
+public class LibraryFineCalculator {
+    public static void main(String[] args) {
+        int daysLate = 8;
+
+        int fine = daysLate * 5;
+
+        System.out.println("Fine = ₹" + fine);
+    }
+}
