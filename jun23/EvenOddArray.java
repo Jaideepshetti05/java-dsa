e464db8c284a37afd24ce92983a641ed89f7e112
Jaideep <jaideepshetti55@gmail.com>
2026-06-23 22:37:50 +0530

    commit 7

---
+++ jun23/EvenOddArray.java
@@ -0,0 +1,12 @@
+public class EvenOddArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        for(int n : arr) {
+            if(n % 2 == 0)
+                System.out.println(n + " Even");
+            else
+                System.out.println(n + " Odd");
+        }
+    }
+}
