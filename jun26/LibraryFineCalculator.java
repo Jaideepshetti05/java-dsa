7ea97d03d0d6cc06d3a6bc6eeed81050f933aee0
Jaideep <jaideepshetti55@gmail.com>
2026-06-26 18:21:56 +0530

    commit -2

---
+++ jun26/LibraryFineCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LibraryFineCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Late days: ");
+        int days = sc.nextInt();
+
+        int fine = 0;
+
+        if (days <= 5)
+            fine = days * 2;
+        else if (days <= 10)
+            fine = days * 5;
+        else
+            fine = days * 10;
+
+        System.out.println("Fine = ₹" + fine);
+
+        sc.close();
+    }
+}
