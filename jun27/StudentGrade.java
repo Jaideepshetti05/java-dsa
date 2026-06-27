bb4862ac412c35587e742b9e97253ceff48255fe
Jaideep <jaideepshetti55@gmail.com>
2026-06-27 17:55:41 +0530

    commit 5

---
+++ jun27/StudentGrade.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StudentGrade {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        int marks=sc.nextInt();
+
+        if(marks>=90)
+            System.out.println("A");
+        else if(marks>=80)
+            System.out.println("B");
+        else if(marks>=70)
+            System.out.println("C");
+        else
+            System.out.println("Fail");
+    }
+}
