20e8e5bbf5118aa4bcf2784b8908b1606111a253
Jaideep <jaideepshetti55@gmail.com>
2026-06-27 17:55:01 +0530

    commit 3

---
+++ jun27/CountVowels.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.nextLine().toLowerCase();
+
+        int count=0;
+
+        for(char c:s.toCharArray()){
+            if("aeiou".indexOf(c)!=-1)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
