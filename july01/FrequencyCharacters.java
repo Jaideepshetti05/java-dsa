2ab66698b9250f5bd94cf81e211703a35d89b234
Jaideep <jaideepshetti55@gmail.com>
2026-07-01 22:38:01 +0530

    commit 8

---
+++ july01/FrequencyCharacters.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FrequencyCharacters {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.nextLine();
+
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(char c:s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+
+        System.out.println(map);
+
+        sc.close();
+    }
+}
