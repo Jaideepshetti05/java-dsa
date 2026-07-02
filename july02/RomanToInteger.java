5cf23b1aa0f1a9249343801b91aea6f123e83898
Jaideep <jaideepshetti55@gmail.com>
2026-07-02 08:06:24 +0530

    commit 1

---
+++ july02/RomanToInteger.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class StudentGradeManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int n = sc.nextInt();
+
+        int total = 0;
+
+        for(int i=1;i<=n;i++){
+            System.out.print("Marks "+i+": ");
+            total += sc.nextInt();
+        }
+
+        double avg = total/(double)n;
+
+        System.out.println("Average = "+avg);
+
+        if(avg>=90)
+            System.out.println("Grade A");
+        else if(avg>=75)
+            System.out.println("Grade B");
+        else if(avg>=60)
+            System.out.println("Grade C");
+        else
+            System.out.println("Fail");
+    }
+}
