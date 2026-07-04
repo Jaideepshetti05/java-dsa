28307388eb68fe0d038eb50f5665c264e7d8b472
Jaideep <jaideepshetti55@gmail.com>
2026-07-04 22:41:02 +0530

    coomit 9

---
+++ july04/PrimeFactors.java
@@ -0,0 +1,17 @@
+public class PrimeFactors {
+
+    public static void main(String[] args){
+
+        int n=360;
+
+        for(int i=2;i<=n;i++){
+
+            while(n%i==0){
+                System.out.print(i+" ");
+                n/=i;
+            }
+
+        }
+
+    }
+}
