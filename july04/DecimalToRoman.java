3654fbe3de23e91992d54bbf3fe3b0527f003c38
Jaideep <jaideepshetti55@gmail.com>
2026-07-04 22:40:27 +0530

    coomit 7

---
+++ july04/DecimalToRoman.java
@@ -0,0 +1,21 @@
+public class DecimalToRoman {
+
+    static int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    static String romans[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    public static void main(String[] args){
+
+        int n=1994;
+
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            while(n>=values[i]){
+                sb.append(romans[i]);
+                n-=values[i];
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
