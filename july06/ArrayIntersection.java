cae3dce35714fe678ae4c75d72ea0fd2cb2db831
Jaideep <jaideepshetti55@gmail.com>
2026-07-06 08:12:35 +0530

    commit 9

---
+++ july06/ArrayIntersection.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class ArrayIntersection {
+
+    public static void main(String[] args){
+
+        int[] a={1,2,3,4,5};
+        int[] b={3,4,5,6};
+
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int x:a)
+            set.add(x);
+
+        for(int x:b)
+            if(set.contains(x))
+                System.out.println(x);
+    }
+}
