79c4affad06cf8f35cef1dbfe8dc0bc5340f870d
Jaideep <jaideepshetti55@gmail.com>
2026-07-08 08:07:08 +0530

    commit 7

---
+++ july08/ObjectCounter.java
@@ -0,0 +1,20 @@
+class Student{
+
+    static int count=0;
+
+    Student(){
+        count++;
+    }
+}
+
+public class ObjectCounter{
+
+    public static void main(String[] args){
+
+        new Student();
+        new Student();
+        new Student();
+
+        System.out.println(Student.count);
+    }
+}
