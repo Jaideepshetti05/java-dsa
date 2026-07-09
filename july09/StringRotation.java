c79163d9a145dfb64b265fc0cd08398c19397770
Jaideep <jaideepshetti55@gmail.com>
2026-07-09 20:27:07 +0530

    commit 10

---
+++ july09/StringRotation.java
@@ -0,0 +1,9 @@
+public class StringRotation {
+    public static void main(String[] args) {
+
+        String s1="abcd";
+        String s2="cdab";
+
+        System.out.println((s1+s1).contains(s2));
+    }
+}
