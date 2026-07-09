0143ef5ecba25653af22cbfd75d5629aa239fc26
Jaideep <jaideepshetti55@gmail.com>
2026-07-09 20:26:29 +0530

    commit 8

---
+++ july09/ArmstrongRange.java
@@ -0,0 +1,18 @@
+public class ArmstrongRange {
+    public static void main(String[] args) {
+
+        for(int n=100;n<=999;n++){
+
+            int temp=n,sum=0;
+
+            while(temp>0){
+                int d=temp%10;
+                sum+=d*d*d;
+                temp/=10;
+            }
+
+            if(sum==n)
+                System.out.println(n);
+        }
+    }
+}
