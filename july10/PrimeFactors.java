1d2ccd673711b396cd90ffba265eb93c9fd81a24
Jaideep <jaideepshetti55@gmail.com>
2026-07-10 18:57:53 +0530

    commit 8

---
+++ july10/PrimeFactors.java
@@ -0,0 +1,19 @@
+public class PrimeFactors {
+
+    public static void main(String[] args){
+
+        int n=360;
+
+        for(int i=2;i*i<=n;i++){
+
+            while(n%i==0){
+
+                System.out.print(i+" ");
+
+                n/=i;
+            }
+        }
+
+        if(n>1) System.out.print(n);
+    }
+}
