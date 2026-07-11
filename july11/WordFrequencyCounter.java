289ad841c58dd80323207b0c64cb54e2cb6749ad
Jaideep <jaideepshetti55@gmail.com>
2026-07-11 22:11:29 +0530

    commit 1

---
+++ july11/WordFrequencyCounter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class WordFrequencyCounter {
+    public static void main(String[] args) {
+        String text = "java is fun java is powerful";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : text.split(" ")) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
