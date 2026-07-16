6f03367d610009f96ec02242cbcc84db851127bb
Jaideep <jaideepshetti55@gmail.com>
2026-07-16 17:52:17 +0530

    commit 10

---
+++ july16/CharacterOccurrence.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class CharacterOccurrence {
+
+    public static void main(String[] args) {
+
+        String s="programming";
+
+        Map<Character,Integer> map=new HashMap<>();
+
+        for(char c:s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+
+        System.out.println(map);
+    }
+}
