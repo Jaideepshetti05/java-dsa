5bd238a83a93b54453f47be37776183b02fb5002
Jaideep <jaideepshetti55@gmail.com>
2026-07-17 22:11:10 +0530

    commit 10

---
+++ july17/RemoveDuplicateCharacters.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class RemoveDuplicateCharacters {
+    public static void main(String[] args) {
+        String s="programming";
+
+        Set<Character> set=new LinkedHashSet<>();
+
+        for(char c:s.toCharArray())
+            set.add(c);
+
+        for(char c:set)
+            System.out.print(c);
+    }
+}
