592618dbaca48e915e46d6598ee6fa031d2cd5b4
Jaideep <jaideepshetti55@gmail.com>
2026-07-18 21:02:24 +0530

    commit 8

---
+++ july18/BitCount.java
@@ -0,0 +1,15 @@
+public class BitCount {
+
+    public static void main(String[] args){
+
+        int n=29;
+        int count=0;
+
+        while(n>0){
+            count+=n&1;
+            n>>=1;
+        }
+
+        System.out.println(count);
+    }
+}
