2711c73c2393460e8df0f57244d06ed7d6994719
Jaideep <jaideepshetti55@gmail.com>
2026-07-18 21:01:41 +0530

    commit 6

---
+++ july18/LongestWord.java
@@ -0,0 +1,16 @@
+public class LongestWord {
+    public static void main(String[] args) {
+
+        String s="Java programming language is powerful";
+
+        String[] words=s.split(" ");
+
+        String longest="";
+
+        for(String w:words)
+            if(w.length()>longest.length())
+                longest=w;
+
+        System.out.println(longest);
+    }
+}
