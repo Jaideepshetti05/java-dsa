f9734bbaaf365d297302cf5fe76ecb9ac2b485dd
Jaideep <jaideepshetti55@gmail.com>
2026-07-31 20:49:35 +0530

    commit 7

---
+++ july31/FirstUniqueCharacter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FirstUniqueCharacter {
+
+    public static void main(String[] args) {
+        String s = "programming";
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        for (char c : map.keySet()) {
+            if (map.get(c) == 1) {
+                System.out.println(c);
+                break;
+            }
+        }
+    }
+}
