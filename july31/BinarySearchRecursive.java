8f12e59d7d46da02d549a88b5fc9a62f99f6445d
Jaideep <jaideepshetti55@gmail.com>
2026-07-31 20:48:17 +0530

    commit 4

---
+++ july31/BinarySearchRecursive.java
@@ -0,0 +1,19 @@
+public class BinarySearchRecursive {
+
+    static int search(int[] arr, int l, int r, int key) {
+        if (l > r)
+            return -1;
+        int mid = (l + r) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        if (key < arr[mid])
+            return search(arr, l, mid - 1, key);
+        return search(arr, mid + 1, r, key);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 6, 8, 10, 12 };
+        System.out.println(search(arr, 0, arr.length - 1, 8));
+    }
+}
