54e0663c82f1ffb53c9056289bd7e880020b3859
Jaideep <jaideepshetti55@gmail.com>
2026-08-01 21:35:20 +0530

    commit 7

---
+++ aug01/FindMissingNumber.java
@@ -0,0 +1,19 @@
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 5 };
+
+        int n = 5;
+
+        int sum = n * (n + 1) / 2;
+
+        int actual = 0;
+
+        for (int i : arr)
+            actual += i;
+
+        System.out.println(sum - actual);
+
+    }
+}
