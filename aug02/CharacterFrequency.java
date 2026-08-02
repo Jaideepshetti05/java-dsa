5c9bbfc21d12d4f52b9e0bcee3769a8041c8cc24
Jaideep <jaideepshetti55@gmail.com>
2026-08-02 21:44:00 +0530

    commit 5

---
+++ aug02/CharacterFrequency.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class CharacterFrequency {
+
+    public static void main(String[] args) {
+
+        String s = "programming";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        System.out.println(map);
+    }
+}
