f2ef79bbe9ce159c0d3f7f74f05aa681e8248348
Jaideep <jaideepshetti55@gmail.com>
2026-08-02 21:44:59 +0530

    commit 8

---
+++ aug02/CountUpperLower.java
@@ -0,0 +1,21 @@
+public class CountUpperLower {
+
+    public static void main(String[] args) {
+
+        String s = "JavaProgramming123";
+
+        int upper = 0, lower = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (Character.isUpperCase(c))
+                upper++;
+
+            if (Character.isLowerCase(c))
+                lower++;
+        }
+
+        System.out.println("Upper=" + upper);
+        System.out.println("Lower=" + lower);
+    }
+}
