c20e6c0924b69ea15f3d78341f16a0cca492058f
Jaideep <jaideepshetti55@gmail.com>
2026-08-03 19:47:25 +0530

    commit 2

---
+++ aug03/CC213_FindPeakElement.java
@@ -0,0 +1,15 @@
+public class CC213_FindPeakElement {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 20, 4, 1, 0 };
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean left = (i == 0 || arr[i] >= arr[i - 1]);
+            boolean right = (i == arr.length - 1 || arr[i] >= arr[i + 1]);
+
+            if (left && right) {
+                System.out.println("Peak = " + arr[i]);
+                break;
+            }
+        }
+    }
+}
