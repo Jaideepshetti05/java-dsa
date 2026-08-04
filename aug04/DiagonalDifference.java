6de57b9dda6da88ec0110e36db13256263fa6634
Jaideep <jaideepshetti55@gmail.com>
2026-08-04 15:21:33 +0530

    commit 4

---
+++ aug04/DiagonalDifference.java
@@ -0,0 +1,19 @@
+public class DiagonalDifference {
+    public static void main(String[] args) {
+
+        int[][] a = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 9, 8, 9 }
+        };
+
+        int d1 = 0, d2 = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            d1 += a[i][i];
+            d2 += a[i][a.length - 1 - i];
+        }
+
+        System.out.println(Math.abs(d1 - d2));
+    }
+}
