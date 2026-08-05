7577b3f9d2a844eef81c53735bb5e00c34a467b2
Jaideep <jaideepshetti55@gmail.com>
2026-08-05 19:30:00 +0530

    commit 10

---
+++ aug05/ReverseWords.java
@@ -0,0 +1,13 @@
+public class ReverseWords {
+
+    public static void main(String[] args) {
+
+        String sentence = "Java Programming Language";
+
+        String[] words = sentence.split(" ");
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+    }
+}
