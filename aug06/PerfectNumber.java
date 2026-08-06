48816f04a7d4d428eb73a481bc7525e25f174a57
Jaideep <jaideepshetti55@gmail.com>
2026-08-06 21:31:29 +0530

    commit 9

---
+++ aug06/PerfectNumber.java
@@ -0,0 +1,12 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = 28;
+        int sum = 0;
+
+        for (int i = 1; i < num; i++)
+            if (num % i == 0)
+                sum += i;
+
+        System.out.println(sum == num);
+    }
+}
