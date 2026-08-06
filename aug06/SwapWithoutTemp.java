ff5393c5a06325646fa2a466ee2ca658feac085a
Jaideep <jaideepshetti55@gmail.com>
2026-08-06 21:31:49 +0530

    commit 10

---
+++ aug06/SwapWithoutTemp.java
@@ -0,0 +1,11 @@
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        int a = 15, b = 25;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a + " " + b);
+    }
+}
