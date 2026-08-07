3912494bec668398426ebf0b0cf1987b2f055d07
Jaideep <jaideepshetti55@gmail.com>
2026-08-07 15:36:33 +0530

    commit 8

---
+++ aug07/CC238_CheckAnagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class CC238_CheckAnagram {
+
+    public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        System.out.println(Arrays.equals(a, b));
+    }
+}
