6e045bd6956cb040b2b0d9e239ed5a971d72646b
Jaideep <jaideepshetti55@gmail.com>
2026-08-08 19:56:22 +0530

    Commit 10

---
+++ aug08/CC80.java
@@ -0,0 +1,11 @@
+public class CC80 {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "cdab";
+
+        if ((s1 + s1).contains(s2))
+            System.out.println("Rotation");
+        else
+            System.out.println("Not Rotation");
+    }
+}
