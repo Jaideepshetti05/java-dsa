48e1b27cc0469ec63134198b6be17da07c9fe3ff
jaideep shetti <jaideepshetti55@gmail.com>
2026-08-11 21:01:22 +0530

    Commit 10

---
+++ aug11/NextGreaterElement.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 2, 10, 8 };
+        int[] result = new int[arr.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+}
