8e9da58987f6bd43d7c1584339f43fdfbca9f180
jaideep shetti <jaideepshetti55@gmail.com>
2026-08-14 21:13:46 +0530

    commit 2

---
+++ aug14/RotateArrayLeft.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class RotateArrayLeft {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+        int k = 2;
+
+        k %= arr.length;
+
+        for (int r = 0; r < k; r++) {
+            int first = arr[0];
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+
+            arr[arr.length - 1] = first;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
